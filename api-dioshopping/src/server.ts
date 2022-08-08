4566e526480e26e5e545c30e9884d6da860a8210
Diego Cardoso <diegocar448@hotmail.com>
2022-08-07 21:39:51 -0300

    arquivos

---
+++ api-dioshopping/src/server.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+
+import { router } from "./routes";
+
+import "./database";
+
+const app = express();
+
+//resolve parcialmente o problema do cors
+app.use(cors());
+
+app.use(express.json());
+app.use(router)
+
+app.listen(5000, () => {
+    console.log('Server on port: 5000')
+})
---
+++ dioshopping/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Muscle Car",
+      fk_idcategorys: 1,
+      name_product: "Mustang 1970",
+      price: 30008228,
+      image: "./images/produtos/mustan.jfif",
+      id_categorys: 1,
+      name_categorys: "Muscle Car",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "S80",
+      fk_idcategorys: 2,
+      name_product: "Delorian",
+      price: 35000,
+      image: "./images/produtos/delorian.jfif",
+      id_categorys: 1,
+      name_categorys: "Antigo",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "Muscle Car",
+      fk_idcategorys: 1,
+      name_product: "Charger 1970",
+      price: 2382220,
+      image: "./images/produtos/charger.jfif",
+      id_categorys: 1,
+      name_categorys: "Muscle Car",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Muscle Car",
+      fk_idcategorys: 1,
+      name_product: "Challenger 1970",
+      price: 222299.99,
+      image: "./images/produtos/chal.jpg",
+      id_categorys: 2,
+      name_categorys: "Muscle Car",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Antigo",
+      fk_idcategorys: 2,
+      name_product: "Antigo",
+      price: 35000.99,
+      image: "./images/produtos/VW-Fusca.jpg",
+      id_categorys: 2,
+      name_categorys: "Antigo",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Muscle Car",
+      fk_idcategorys: 1,
+      name_product: "Opala",
+      price: 29909.99,
+      image: "./images/produtos/opala.jfif",
+      id_categorys: 2,
+      name_categorys: "Muscle Car",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Muscle Car",
+      fk_idcategorys: 1,
+      name_product: "Maverick 1970",
+      price: 200000,
+      image: "./images/produtos/maver.jfif",
+      id_categorys: 3,
+      name_categorys: "Muscle Car",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Tecnologico",
+      fk_idcategorys: 3,
+      name_product: "Tesla",
+      price: 450000,
+      image: "./images/produtos/tesla.jfif",
+      id_categorys: 3,
+      name_categorys: "Tecnologico",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Antigo",
+      fk_idcategorys: 2,
+      name_product: "Kombi",
+      price: 3500,
+      image: "./images/produtos/transferir (1).jfif",
+      id_categorys: 4,
+      name_categorys: "Antigo",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Antigo",
+      fk_idcategorys: 2,
+      name_product: "Gurgel",
+      price: 399,
+      image: "./images/produtos/gurgelitaipu.jfif",
+      id_categorys: 4,
+      name_categorys: "Antigo",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Eletrico",
+      fk_idcategorys: 3,
+      name_product: "JAC",
+      price: 188000,
+      image: "./images/produtos/jac.jfif",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Eletrico",
+      fk_idcategorys: 3,
+      name_product: "BYD",
+      price: 189999.99,
+      image: "./images/produtos/byd.jfif",
+      id_categorys: 5,
+      name_categorys: "Eletrico",
+      selected: false
+    },
+];
+
+export default productsList;
